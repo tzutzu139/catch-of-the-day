99af47a50ed94fb9e499370cfb8425d0b4954eee
Gabriel Cristian <tzutzus4@gmail.com>
2021-03-18 01:15:28 +0200

    Added functionality to the store selector button.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={ getFunName() } />
         <button type="submit">Visit Store -> </button>
       </form>
     )
