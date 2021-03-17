dd8027eb095adb0c11fd27c8b8f57b635e0a55d7
tzutzu139 <tzutzus4@gmail.com>
2021-03-17 18:28:01 +0200

    Added react-routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import StorePicker from "./components/StorePicker";
 import App from "./components/App";
+import Router from './components/Router';
 import "./css/style.css";
 
-render(<App />, document.getElementById("root"));
+render(<Router />, document.getElementById("root"));
