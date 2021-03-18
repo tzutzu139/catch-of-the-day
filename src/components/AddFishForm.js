92f0063b381a971fb237d791bff80be7610838bf
tzutzu139 <tzutzus4@gmail.com>
2021-03-18 18:24:20 +0200

    Fixed the error

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  createFish = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    console.log("Making a fish");
+  };
   render() {
-    createFish = (event) => {
-      // 1. Stop the form from submitting
-      event.preventDefault();
-      console.log("Making a fish");
-    }
     return (
       <form className="fish-edit" onSubmit={this.createFish}>
         <input name="name" type="text" placeholder="Name" />
@@ -19,7 +19,7 @@ class AddFishForm extends React.Component {
         <input name="image" type="text" placeholder="Image" />
         <button type="submit"> + Add Fish</button>
       </form>
-    )
+    );
   }
 }
 
