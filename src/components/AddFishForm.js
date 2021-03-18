6fc5e327451da52e42c5c07847271c5046d79735
Gabriel Cristian <tzutzus4@gmail.com>
2021-03-18 16:57:36 +0200

    small changes on fish form

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -2,6 +2,11 @@ import React from "react";
 
 class AddFishForm extends React.Component {
   render() {
+    createFish = (event) => {
+      // 1. Stop the form from submitting
+      event.preventDefault();
+      console.log("Making a fish");
+    }
     return (
       <form className="fish-edit" onSubmit={this.createFish}>
         <input name="name" type="text" placeholder="Name" />
