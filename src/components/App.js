85aed1e8a81c9b3ee64329529688f902aa223e19
Gabriel Cristian <tzutzus4@gmail.com>
2021-03-23 00:04:23 +0200

    Bi-directional data flow amd live state editing.

--- src/components/App.js
+++ src/components/App.js
@@ -45,6 +45,15 @@ class App extends React.Component {
     });
   };
 
+  updateFish = (key, updatedFish) => {
+    // 1. Take a copy of the current state
+    const fishes = {...this.state.fishes};
+    // 2. Update that state
+    fishes[key] = updatedFish;
+    // 3. Set that to state
+    this.setState({ fishes: fishes});
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -75,7 +84,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
         addFish={this.addFish}
+        updateFish={this.updateFish}
         loadSampleFishes={this.loadSampleFishes}
+        fishes={this.state.fishes}
         />
       </div>
     )
